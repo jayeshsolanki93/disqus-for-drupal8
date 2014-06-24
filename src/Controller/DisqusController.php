02b6009f0ac24573bc9cc2e417bfd2e62bd994b8
Jayesh Solanki <jayeshsolanki93@gmail.com>
2014-06-24 17:25:36 +0530

    Remove drupal_add_js() from DisqusController.php closeWindow() function returns a render array with the inline JS attached.

--- src/Controller/DisqusController.php
+++ src/Controller/DisqusController.php
@@ -7,10 +7,14 @@ class DisqusController {
    * Menu callback; Automatically closes the window after the user logs in.
    *
    * @return
-   *   Confirmation message and link that closes overlay window.
+   *   A render array containing the confirmation message and link that closes overlay window.
    */
   public function closeWindow() {
-    _drupal_add_js('window.close();', 'inline');
-    return t('Thank you for logging in. Please close this window, or <a href="@clickhere">click here</a> to continue.', array('@clickhere' => 'javascript:window.close();'));
+     $build['#attached']['js'][] = array(
+       'type' => 'inline',
+       'data' => 'window.close();',
+       '#markup' => t('Thank you for logging in. Please close this window, or <a href="!clickhere">click here</a> to continue.', array('!clickhere' => 'javascript:window.close();')),
+     );
+     return $build;
   }
 }
