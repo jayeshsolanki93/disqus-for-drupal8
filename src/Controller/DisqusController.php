b155586829dc09a238b979a9594405143c51febf
Jayesh Solanki <jayeshsolanki93@gmail.com>
2014-06-24 19:01:48 +0530

    Fixed error- moved #markup up a few levels in the render array in DisqusController.

--- src/Controller/DisqusController.php
+++ src/Controller/DisqusController.php
@@ -10,10 +10,16 @@ class DisqusController {
    *   A render array containing the confirmation message and link that closes overlay window.
    */
   public function closeWindow() {
-     $build['#attached']['js'][] = array(
-       'type' => 'inline',
-       'data' => 'window.close();',
-       '#markup' => t('Thank you for logging in. Please close this window, or <a href="!clickhere">click here</a> to continue.', array('!clickhere' => 'javascript:window.close();')),
+     $build = array(
+       '#markup'=> t('Thank you for logging in. Please close this window, or <a href="!clickhere">click here</a> to continue.', array('!clickhere' => 'javascript:window.close();')),
+       '#attached' => array(
+         'js' => array(
+           array(
+             'type' => 'inline',
+             'data' => 'window.close();',
+           ),
+         ),
+       ),
      );
      return $build;
   }
