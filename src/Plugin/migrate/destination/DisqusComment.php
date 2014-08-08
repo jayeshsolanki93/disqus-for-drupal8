b0270c878aa8351f48eede37c0b6e0379ea9052a
Jayesh Solanki <jayeshsolanki93@gmail.com>
2014-08-08 16:54:33 +0530

    Removed entity_id and entity_type fields from destination plugin instead directly using disqus-identifier field and relying on the migration process for preprocessing this field. Included API call for creating a disqus thread when it doesnt exists for the current comment migration.

--- src/Plugin/migrate/destination/DisqusComment.php
+++ src/Plugin/migrate/destination/DisqusComment.php
@@ -10,7 +10,6 @@ namespace Drupal\disqus\Plugin\migrate\destination;
 use Drupal\migrate\Plugin\migrate\destination\DestinationBase;
 use Drupal\migrate\Entity\MigrationInterface;
 use Psr\Log\LoggerInterface;
-use Drupal\Core\Entity\EntityManager;
 use Drupal\Core\Config\ConfigFactoryInterface;
 
 /**
@@ -30,13 +29,6 @@ class DisqusComment extends DestinationBase {
   protected $logger;
 
   /**
-   * The entity manager service.
-   *
-   * @var \Drupal\Core\Entity\EntityManager
-   */
-  protected $entityManager;
-
-  /**
    * The disqus.settings configuration.
    *
    * @var \Drupal\Core\Config\Config
@@ -54,16 +46,13 @@ class DisqusComment extends DestinationBase {
    *   The plugin implemetation definition.
    * @param \Drupal\migrate\Entity\MigrationInterface $migration
    *   The migration.
-   * @param \Drupal\Core\Entity\EntityManager $entity_manager
-   *   The entity manager service.
    * @param \Psr\Log\LoggerInterface $logger
    *   A logger instance.
    * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
    *   The config factory.
    */
-  public function __construct(array $configuration, $plugin_id, $plugin_definition, MigrationInterface $migration, EntityManager $entity_manager, LoggerInterface $logger, ConfigFactoryInterface $config_factory) {
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, MigrationInterface $migration, LoggerInterface $logger, ConfigFactoryInterface $config_factory) {
     parent::__construct($configuration, $plugin_id, $plugin_definition, $migration);
-    $this->entityManager = $entity_manager;
     $this->logger = $logger;
     $this->config = $config_factory->get('disqus.settings');
   }
@@ -73,7 +62,6 @@ class DisqusComment extends DestinationBase {
    */
   public static function create(ContainerInterface $container) {
     return new static(
-      $container->get('entity.manager'),
       $container->get('logger.factory')->get('disqus'),
       $container->get('config.factory')
     );
@@ -86,8 +74,8 @@ class DisqusComment extends DestinationBase {
     return array(
       'message' => $this->t('The comment body.'),
       'parent' => $this->t('Parent comment ID. If set to null, this comment is not a reply to an existing comment.'),
-      'entity_id' => $this->t('The entity to which this comment belongs.'),
-      'entity_type' => $this->t('The entity-type of the entity on which this comment belongs.'),
+      'identifier' => $this->t('The disqus identifier to look up the correct thread.'),
+      'title' => $this->t("The title of the comment's thread page."),
       'author_email' => $this->t("The comments author's email."),
       'author_name' => $this->t("The comments author's name."),
       'author_url' => $this->t("The comments author's url."),
@@ -108,24 +96,22 @@ class DisqusComment extends DestinationBase {
    * {@inheritdoc}
    */
   public function import(Row $row, array $old_destination_id_values = array()) {
-    $entity_type = $row->getDestinationProperty('entity_type');
-    $entity_id = $row->getDestinationProperty('entity_id');
+    $identifier = $row->getDestinationProperty('identifier');
     $disqus = disqus_api();
     if ($disqus) {
       try {
-        $thread = $disqus->threads->details(array('forum' => $this->config->get('disqus_domain'), 'thread:ident' => "{$entity_type}/{$entity_id}", 'thread' => '1'));
+        $thread = $disqus->threads->details(array('forum' => $this->config->get('disqus_domain'), 'thread:ident' => $identifier, 'thread' => '1'));
       }
       catch (Exception $exception) {
-        $this->logger->error('Error loading thread details for entity : !identifier. Check your API keys.', array('!identifier' => "{$entity_type}/{$entity_id}"));
+        $this->logger->error('Error loading thread details for entity : !identifier. Check your API keys.', array('!identifier' => $identifier));
         $thread = null;
       }
-      $entity = $this->entityManager->getStorage($entity_type)->load($entity_id);
-
       if(!isset($thread->id)) {
         try {
+          $thread = $disqus->threads->create(array('forum' =>  $this->config->get('disqus_domain'), 'access_token' => $this->config->get('advanced.disqus_useraccesstoken'), 'title' => $row->getDestinationProperty('title'),  'identifier' => $identifier));
         }
         catch (Exception $exception) {
-          $this->logger->error('Error creating thread for entity : !identifier. Check your user access token.', array('!identifier' => "{$entity_type}/{$entity_id}"));
+          $this->logger->error('Error creating thread for entity : !identifier. Check your user access token.', array('!identifier' => $identifier));
         }
       }
       try {
