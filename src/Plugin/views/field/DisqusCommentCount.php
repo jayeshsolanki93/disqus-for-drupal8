249a0730a3934e6e9de795fbeb111c50cea3fa33
Jayesh Solanki <jayeshsolanki93@gmail.com>
2014-08-12 01:20:40 +0530

    Issue #2289303: First commit for porting 'Disqus comment count' view. For creating a view, just checking if the node has the disqus-comment field

--- src/Plugin/views/field/DisqusCommentCount.php
+++ src/Plugin/views/field/DisqusCommentCount.php
@@ -5,62 +5,114 @@ namespace Drupal\disqus\Plugin\views\field;
 use Drupal\views\Plugin\views\field\FieldPluginBase;
 use Drupal\Component\Annotation\PluginID;
 use Drupal\views\ResultRow;
+use \Drupal\node\Entity\Node;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\disqus\DisqusCommentManager;
 
 /**
  * Field handler to present the number of Disqus comments on a node.
  *
  * @ingroup views_field_handlers
  *
- * @PluginID("disqus_comment_count")
+ * @ViewsField("disqus_comment_count")
  */
 class DisqusCommentCount extends FieldPluginBase {
+
+  /**
+   * The current user.
+   *
+   * @var \Drupal\Core\Session\AccountInterface
+   */
+  protected $currentUser;
+
+  /**
+   * Disqus comment manager object.
+   *
+   * @var \Drupal\disqus\DisqusCommentManager
+   */
+  protected $disqusManager;
+
+  /**
+   * Constructs a 'Disqus Comment Count' view field plugin.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Session\AccountInterface $current_user
+   *   The current user.
+   * @param \Drupal\disqus\DisqusCommentManager $disqusManager
+   *   The disqus comment manager object.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, AccountInterface $current_user, DisqusCommentManager $disqus_manager) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->currentUser = $current_user;
+    $this->disqusManager = $disqus_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('current_user'),
+      $container->get('disqus.manager')
+    );
+  }
+
   /**
    * {@inheritdoc}
    */
   function render(ResultRow $values) {
     // Ensure Disqus comments are available on the node user has access to edit this node.
-    $node = node_load($values->nid);
-
-    if (!\Drupal::currentUser()->hasPermission('view disqus comments') || isset($node->disqus)) {
+    $node = Node::load($values->nid);
+    $field = $this->disqusManager->getFields('node');
+    if(!$node->hasField(key($field))) {
       return;
     }
-
-    // Extract the Disqus values.
-    $disqus = $node->disqus;
-
-    // Build a renderable array for the link.
-    $content = array(
-      '#theme' => 'link',
-      '#text' => t('Comments'),
-      '#path' => $disqus['identifier'],
-      '#options' => array(
+    if ($node->get(key($field))->status && $this->currentUser->hasPermission('view disqus comments')) {
+      // Build a renderable array for the link.
+      $links['disqus_comments_num'] = array(
+        'title' => t('Comments'),
+        'href' => $node->url('canonical',array('absolute' => TRUE)),
         'fragment' => 'disqus_thread',
         'attributes' => array(
           // Identify the node for Disqus with the unique identifier:
           // http://docs.disqus.com/developers/universal/#comment-count
-          'data-disqus-identifier' => $disqus['identifier'],
+          'data-disqus-identifier' => 'node/' . $node->id(),
         ),
-        'html' => FALSE,
-      ),
-    );
-
-    /**
-     * This attaches disqus.js, which will look for the DOM variable
-     * disqusComments which is set below. When found, the disqus javascript
-     * api replaces the html element with the attribute:
-     * "data-disqus-identifier" and replaces the element with the number of
-     * comments on the node.
-     */
-    $content['#attached'] = array(
-      'js' => array(
-        array('data' => drupal_get_path('module', 'disqus') . '/disqus.js'),
-        array(
-          'data' => array('disqusComments' => $disqus['domain']),
-          'type' => 'setting',
+      );
+      $content = array(
+        '#theme' => 'links',
+        '#links' => $links,
+        '#attributes' => array(
+          'class' => array('links', 'inline'),
         ),
-      ),
-    );
+      );
 
-    return drupal_render($content);
+      /**
+       * This attaches disqus.js, which will look for the DOM variable
+       * disqusComments which is set below. When found, the disqus javascript
+       * api replaces the html element with the attribute:
+       * "data-disqus-identifier" and replaces the element with the number of
+       * comments on the node.
+       */
+      $content['#attached'] = array(
+        'js' => array(
+          array('data' => drupal_get_path('module', 'disqus') . '/disqus.js'),
+          array(
+            'data' => array('disqusComments' => \Drupal::config('disqus.settings')->get('disqus_domain')),
+            'type' => 'setting',
+          ),
+        ),
+      );
+      return drupal_render($content);
+    }
   }
 }
